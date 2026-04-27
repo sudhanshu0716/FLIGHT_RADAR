0a7245424e59a363ef239377060ab441533cf953
sudhanshu0716 <122021740+sudhanshu0716@users.noreply.github.com>
2026-04-28 00:10:41 +0530

    Fix Express 5 wildcard route syntax

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use('/api-live', createProxyMiddleware({
 // Serve React static files in production
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
